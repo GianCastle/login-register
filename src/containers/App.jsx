c97085e30b83b476eeafdc87a66acff20f8c0ab1
Edisson Mata <edissondevs2000@gmail.com>
2019-10-26 14:51:29 -0400

    Add logic in the App Container

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import SmallCard from '../components/SmallCard'
 import CardTabs from '../components/CardTabs'
 
 const App = props => {
+    // State
+    const [actual, setActual] = useState('Login');
+
+    // Events handlers
+    const changeActual = (item) => {
+        return () => setActual(item);
+    }
     return (
         <>
             <SmallCard>
                 <CardTabs buttons={[
                         {
                             text: "Login",
-                            action: () => {}
+                            action: changeActual("Login")
                         }, 
                         {
                             text: "Register",
-                            action: () => {}
+                            action: changeActual("Register")
                         }
                     ]}
-                    actual={"Login"}
+                    actual={actual}
                 />
             </SmallCard>
         </>
