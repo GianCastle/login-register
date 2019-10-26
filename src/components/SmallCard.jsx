712b298a7b1c04654d9abb451d3bda8910c555e1
Edisson Mata <edissondevs2000@gmail.com>
2019-10-26 14:42:58 -0400

    Proptypes has been integrated with SmallCard component

--- src/components/SmallCard.jsx
+++ src/components/SmallCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import '../assets/css/small-card.css'
 
 const SmallCard = props => {
@@ -9,6 +10,10 @@ const SmallCard = props => {
             }
         </div>
     )
+}
+
+SmallCard.propTypes = {
+    children: PropTypes.any.isRequired,
 }
 
 export default SmallCard
